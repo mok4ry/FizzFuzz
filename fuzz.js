871487255cbf3eba05b9522fa6af0cc1469816c6
Jesse Jurman <j.r.jurman@gmail.com>
2015-09-23 23:51:22 -0400

    breaking changes

--- fuzz.js
+++ fuzz.js
@@ -8,7 +8,24 @@ Browser.visit('http://127.0.0.1:8080/bodgeit/login.jsp', function(e, browser) {
     pressButton("Login", function(err) {
       // check validation text
       var didWork = browser.document.body.textContent;
-      console.log(didWork);
+      var successString = "You have logged in successfully";
+      console.log(didWork.indexOf(successString) != -1);
     });
 
 });
+
+function login(browser, url,
+                userNameSelector, userName,
+                passwordSelector, password,
+                loginSelector, successString) {
+  Browser.visit(url, function(e, browser) {
+    browser.
+      fill(userNameSelector, userName).
+      fill(passwordSelector, password).
+      pressButton(loginSelector, function(err) {
+        // check validation text
+        var didWork = browser.document.body.textContent;
+        console.log(didWork.indexOf(successString) != -1);
+      });
+  });
+}
