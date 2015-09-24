62e7dabccb88a8bc6fdf1425d128ef4d3631b3fd
Jesse Jurman <j.r.jurman@gmail.com>
2015-09-23 22:48:47 -0400

    document . stuff

--- fuzz.js
+++ fuzz.js
@@ -1,7 +1,16 @@
 const Browser = require('zombie');
 
-Browser.visit('http://www.rit.edu/', function(e, browser) {
-  browser.queryAll("a").map(function (node) {
-    console.log(node.href);
-  })
+Browser.visit('http://127.0.0.1:8080/bodgeit/login.jsp', function(e, browser) {
+  // log into "The BodgeIt Store"
+  var userNameNode = browser.document.getElementById("username");
+  userNameNode.value = "foobar@gmail.com";
+  var passwordNode = browser.document.getElementById("password");
+  passwordNode.value = "foobar";
+  var submitNode = browser.document.getElementById("submit");
+  submitNode.click();
+
+  // check validation text
+  var didWork = document.body.textContent.split("You have logged in successfully: ").length > 2
+
+  console.log(didWork);
 });
