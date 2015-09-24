ff452168078fbb5b2256143eec316decff49396b
lance13c <pianoman13c@gmail.com>
2015-09-23 23:49:30 -0400

    dvwa login authentication  Please enter the commit message for your changes. Lines starting

--- fuzz.js
+++ fuzz.js
@@ -1,14 +1,26 @@
 const Browser = require('zombie');
 
-Browser.visit('http://127.0.0.1:8080/bodgeit/login.jsp', function(e, browser) {
-  // log into "The BodgeIt Store"
-  browser.
-    fill("username", "foobar@gmail.com").
-    fill("password", "foobar").
-    pressButton("Login", function(err) {
-      // check validation text
-      var didWork = browser.document.body.textContent;
-      console.log(didWork);
-    });
+const bodgitURL = "http://127.0.0.1:8080/bodgeit/login.jsp";
+const dwvaURL = "http://127.0.0.1:7000/dvwa/login.php";
 
-});
+var usernameB = "foobar@gmail.com"; // bodgit user
+var passwordB = "foobar"; // bodgit password
+
+var usernameD = "admin";
+var passwordD = "password";
+
+
+//The string: "You have logged in as 'admin'" --- the class: "message"
+
+  Browser.visit(dwvaURL, function(e, browser) {
+    // log into "The BodgeIt Store"
+    browser.
+      fill("username", usernameD).
+      fill("password", passwordD).
+      pressButton("Login", function(err) {
+        // check validation text
+        var didWork = browser.document.body.textContent;
+        console.log(didWork);
+      });
+
+  });
